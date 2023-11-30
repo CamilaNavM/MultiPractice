775897399714cfd61029daaddaabe9e0c3dd011d
CamilaNavM <144072023+CamilaNavM@users.noreply.github.com>
2023-11-30 01:23:20 -0600

    cambios en UI

--- Multiplayer Practice/Assets/Scripts/CreateAndJoinRooms.cs
+++ Multiplayer Practice/Assets/Scripts/CreateAndJoinRooms.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
-public class CreateAndJoinRooms : MonoBehaviour
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+
 {
     public InputField createInput;
     public InputField joinInput;
-
+    
     public void CreateRoom()
     {
-        PhotonNetwork.JoinRoom(joinInput.text);
+        PhotonNetwork.CreateRoom(createInput.text);
     }
 
     public void JoinRoom()
     {
         PhotonNetwork.JoinRoom(joinInput.text);
     }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
 
     // Start is called before the first frame update
     void Start()
